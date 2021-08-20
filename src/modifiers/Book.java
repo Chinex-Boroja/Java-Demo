95d12ccfafc8378b59643dbec1928312aef9c2d5
Ihedioha Chinedu Innocent <ihediohachinedu21@gmail.com>
2021-08-20 15:13:49 +0100

    adding modifiers to fields

---
+++ src/modifiers/Book.java
@@ -0,0 +1,21 @@
+package modifiers;
+
+public class Book {
+    private String title;
+    private String author;
+    private boolean isBorrowed;
+
+    public Book(String title, String author) {
+        this.title = title;
+        this.author = author;
+    }
+    public void borrowedBook() {
+        isBorrowed = true;
+    }
+    public void returnBook() {
+        isBorrowed = false;
+    }
+    public boolean isBookBorrowed() {
+        return isBorrowed;
+    }
+}
